d1fe36abe42f8a7a3fd74d10c97ed4c3b9e94553
kuldeepsingh1828 <kuldeepsingh1828@gmail.com>
2023-06-13 22:35:13 +0530

    task 1 Updated

--- src/components/MyStateFullComponent/MyStateFullComponent.jsx
+++ src/components/MyStateFullComponent/MyStateFullComponent.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import MyComponent from '../MyComponent';
 export default function MyStateFullComponent() {
     let [state, setState] = useState([]);
-    console.log('state', state);
     let clickHandler = () => {
         let todo = document.getElementById("input").value;
         setState([...state, todo]);
